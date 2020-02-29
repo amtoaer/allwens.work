f308719012bab446e08fdf490f901bb37d000fdd
JeasonLau <b1361974998@gmail.com>
2020-02-29 20:54:12 +0800

    Site updated: 2020-02-29 20:54:11

--- js/cheat.js
+++ js/cheat.js
@@ -2,12 +2,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/images/failure.ico");
+        $('[rel="icon"]').attr('href', "https://allwens-work.oss-cn-beijing.aliyuncs.com/failure.ico");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/images/favicon.ico");
+        $('[rel="icon"]').attr('href', "https://allwens-work.oss-cn-beijing.aliyuncs.com/favicon.ico");
         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
