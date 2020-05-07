8f371615180768890335aea9fb25bf92445d1ca8
JeasonLau <b1361974998@gmail.com>
2020-05-07 18:45:22 +0800

    Site updated: 2020-05-07 18:45:21

---
+++ js/pisces.js
@@ -0,0 +1,58 @@
+function affix() {
+
+  var sidebarInner = $('.sidebar-inner');
+
+  initAffix();
+  resizeListener();
+
+  function initAffix() {
+    var headerOffset = getHeaderOffset(),
+      // footerOffset = getFooterOffset(),
+      sidebarHeight = $('#sidebar').height(),
+      contentHeight = $('#content').height();
+
+    // Not affix if sidebar taller then content (to prevent bottom jumping).
+    if (sidebarHeight < contentHeight) {
+      sidebarInner.affix({
+        offset: {
+          top: headerOffset - 2 * 52,
+          bottom: 460
+        }
+      });
+    }
+
+    // setSidebarMarginTop(headerOffset).css({ 'margin-left': 'initial' });
+  }
+
+  function resizeListener() {
+    var mql = window.matchMedia('(min-width: 991px)');
+    mql.addListener(function (e) {
+      if (e.matches) {
+        recalculateAffixPosition();
+      }
+    });
+  }
+
+  function getHeaderOffset() {
+    return $('.header-inner').height() + 52;
+  }
+
+  // function getFooterOffset () {
+  //   var footerInner = $('.footer-inner'),
+  //       footerMargin = footerInner.outerHeight(true) - footerInner.outerHeight(),
+  //       footerOffset = footerInner.outerHeight(true) + footerMargin;
+  //   return footerOffset;
+  // }
+
+  function setSidebarMarginTop(headerOffset) {
+    return $('#sidebar').css({ 'margin-top': headerOffset });
+  }
+
+  function recalculateAffixPosition() {
+    $(window).off('.affix');
+    sidebarInner.removeData('bs.affix').removeClass('affix affix-top affix-bottom');
+    initAffix();
+  }
+
+}
+affix();
