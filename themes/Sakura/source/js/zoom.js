7ae8c94867adbdffbde1caa191d0362e46d375dd
amtoaer <amtoaer@outlook.com>
2020-12-17 11:47:47 +0800

    只保留主页以更新google索引

--- themes/Sakura/source/js/zoom.js
+++
@@ -1,4 +0,0 @@
-window.zoomImages = function () {
-    $('.entry-content img').each(function () { $(this).wrap('<a data-fancybox="images" href=' + this.getAttribute('src') + '></a>') })
-}
-window.zoomImages()
